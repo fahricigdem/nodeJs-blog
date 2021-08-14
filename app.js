6dc9cd3b8aea27aece5d1775ef4db9e7652aa072
FAHRI CIGDEM <64100513+fahricigdem@users.noreply.github.com>
2021-08-14 19:23:48 +0200

    mongo cloud db - mongoose - connect

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const morgan = require('morgan');
-
+const mongoose = require('mongoose');
 const app = express();
-app.set('view engine', 'ejs');
 
-app.listen(3000)
+const dbURL = 'mongodb+srv://fahri:asd123@nodeblog.i6tpq.mongodb.net/node-blog?retryWrites=true&w=majority'
+mongoose.connect(dbURL, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then((result) => app.listen(3000))
+    .catch((err) => console.log(err))
+
+app.set('view engine', 'ejs');
 
 app.use(express.static('public'))
 app.use(morgan('dev'))
